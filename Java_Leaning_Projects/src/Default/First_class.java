852f2fa50b4ab0759755dbdbcf423f42d26492e8
Blazej <badyl00.sudra@gmail.com>
2024-10-11 18:18:53 +0200

    Commit from Kraken

--- Java_Leaning_Projects/src/Default/First_class.java
+++ Java_Leaning_Projects/src/Default/First_class.java
@@ -3,7 +3,7 @@ package Default;
 public class First_class {
 	public static void main(String[] args) {
 		System.out.println("Hello World Kolega tu by³ ale remake");
-
+		System.out.println("Test Krakena");
 
 	}
 }
