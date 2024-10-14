265d2f0fd118c69ca4ed96f1eb6ac775e204da64
Micromation <badyl00.sudra@gmail.com>
2024-10-14 11:54:09 +0200

    First commit from another laptop - TEST

---
+++ Pierwszy_projekt/src/pl/javastart/first/PierwszaKlasa.java
@@ -0,0 +1,24 @@
+package pl.javastart.first;
+
+import java.awt.Frame;
+
+import javax.swing.JComponent;
+
+public class PierwszaKlasa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		Frame frame = new Frame("Puste okno");
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+        
+        frame.addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                System.exit(0);
+            }
+            
+        });
+
+}
+}
